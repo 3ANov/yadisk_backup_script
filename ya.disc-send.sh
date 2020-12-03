77d4d457fcedbe2d59da277933f247ccccbf5355
3ANov <3anovdev@gmail.com>
2020-12-03 15:02:43 +0300

    Некоторые изменения в файлах

--- ya.disc-send.sh
+++ ya.disc-send.sh
@@ -4,9 +4,8 @@ BACKUP_DIR=$HOME'/1c_backup'
 YADISC_DIR='disk:/server_backups/'
  
 # найти последний .zip файл в папке бэкапов
-LAST_BACKUP=$(ls -t $BACKUP_DIR/.zip | head -1)
+LAST_BACKUP=$(ls -t $BACKUP_DIR/*.zip | head -1)
  
-LAST_BACKUP_PATH=$BACKUP_DIR$LAST_BACKUP
  
 if [ -z $LAST_BACKUP ]
 then
@@ -15,8 +14,8 @@ then
 fi
  
  
-/usr/bin/ydcmd put $LAST_BACKUP_PATH $YADISC_DIR
+/usr/bin/ydcmd put $LAST_BACKUP $YADISC_DIR
  
-rm -f $LAST_BACKUP_PATH
+rm -f $LAST_BACKUP
  
 echo 'done!'
