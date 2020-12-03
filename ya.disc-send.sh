cdf26dcce29e72d0ca980980be030554c4122f15
3ANov <3anovdev@gmail.com>
2020-12-03 16:18:46 +0300

    малнькая ошибочка

--- ya.disc-send.sh
+++ ya.disc-send.sh
@@ -4,7 +4,7 @@ BACKUP_DIR=$HOME'/1c_backup'
 YADISC_DIR='disk:/server_backups/'
  
 # найти последний .zip файл в папке бэкапов
-LAST_BACKUP=$(ls -t $BACKUP_DIR/*.zip | head -1)
+LAST_BACKUP=$(ls -t $BACKUP_DIR/*.tar.gz | head -1)
  
  
 if [ -z $LAST_BACKUP ]
