b1fa8c2757c45b25568eba26b9c79c0f9294da41
3ANov <3anovdev@gmail.com>
2020-12-08 19:08:17 +0300

    удалил лишнее

--- ya.disc-sync.sh
+++ ya.disc-sync.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
  
 BACKUP_DIR=$HOME'/1c_backup'
-YADISC_DIR='disk:/1c_backup/'
- 
-# найти последний .zip файл в папке
-LAST_DB_FILE=$(ls -t $BACKUP_DIR/*_AA_BB.zip | head -1)
- 
- 
-if [ -z $LAST_DB_FILE ]
-then
-    echo -e "Бэкапов для отправки на ya.disk не найдено\n"
-    exit
-fi
- 
+YADISC_DIR='disk:/1c_backup/' 
  
 ydcmd get $YADISC_DIR $BACKUP_DIR --timeout=300 --retries=3 
 
