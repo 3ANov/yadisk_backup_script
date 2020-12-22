93ea36fb28f7eb980bce28954318dbcc60282c68
3ANov <3anovdev@gmail.com>
2020-12-22 23:30:13 +0300

    Чуть переделан скрипт синхронизации

--- ya.disc-sync.sh
+++ ya.disc-sync.sh
@@ -5,15 +5,20 @@ YADISC_DIR='disk:/1c_backup'
 
 # !!! Исправить расширение !!!
 # Сообщение с главного сервера на второстепенный (загружается в диск)
-FROM_MAIN_TO_SLAVE_MESSAGE=$BACKUP_DIR/'message_aa_bb.txt'
+FROM_MAIN_TO_SLAVE_MESSAGE_LOCAL=$BACKUP_DIR/'message_aa_bb.txt'
+FROM_MAIN_TO_SLAVE_MESSAGE_YADISK=$YADISC_DIR/'message_aa_bb.txt'
+
+
 
 # !!! Исправить расширение !!!
 # Сообщение с второстепенного сервера на главный (выгружается с диска)
-FROM_SLAVE_TO_MAIN_MESSAGE=$YADISC_DIR/'message_bb_aa.txt'
+FROM_SLAVE_TO_MAIN_MESSAGE_YADISK=$YADISC_DIR/'message_bb_aa.txt'
+FROM_SLAVE_TO_MAIN_MESSAGE_LOCAL=$BACKUP_DIR/'message_bb_aa.txt'
 
-ydcmd put $FROM_MAIN_TO_SLAVE_MESSAGE --timeout=300 --retries=3 
 
-ydcmd get $FROM_SLAVE_TO_MAIN_MESSAGE --timeout=300 --retries=3 
+
+ydcmd put $FROM_MAIN_TO_SLAVE_MESSAGE_LOCAL $FROM_MAIN_TO_SLAVE_MESSAGE_YADISK --timeout=300 --retries=3 
+ydcmd get $FROM_SLAVE_TO_MAIN_MESSAGE_YADISK $FROM_SLAVE_TO_MAIN_MESSAGE_LOCAL --timeout=300 --retries=3 
 
  
 echo 'done!'
