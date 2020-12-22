039523b7995a5f56b7554990c7cc8f19719f3f18
3ANov <3anovdev@gmail.com>
2020-12-22 23:05:31 +0300

    Переделан скрипт синхронизации

--- ya.disc-sync.sh
+++ ya.disc-sync.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
  
 BACKUP_DIR=$HOME'/1c_backup'
-YADISC_DIR='disk:/1c_backup/' 
- 
-ydcmd get $YADISC_DIR $BACKUP_DIR --timeout=300 --retries=3 
+YADISC_DIR='disk:/1c_backup' 
 
-ydcmd --rsync put $BACKUP_DIR --timeout=300 --retries=3 
+# !!! Исправить расширение !!!
+# Сообщение с главного сервера на второстепенный (загружается в диск)
+FROM_MAIN_TO_SLAVE_MESSAGE=$BACKUP_DIR/'message_aa_bb.txt'
+
+# !!! Исправить расширение !!!
+# Сообщение с второстепенного сервера на главный (выгружается с диска)
+FROM_SLAVE_TO_MAIN_MESSAGE=$YADISC_DIR/'message_bb_aa.txt'
+
+ydcmd put $FROM_MAIN_TO_SLAVE_MESSAGE --timeout=300 --retries=3 
+
+ydcmd get $FROM_SLAVE_TO_MAIN_MESSAGE --timeout=300 --retries=3 
 
  
 echo 'done!'
